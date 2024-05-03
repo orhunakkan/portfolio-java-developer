a4c6b0512dae3c4f34df0e3f036b12e4d2b1a70e
orhunakkan <orhunakkan@outlook.com>
2024-05-03 12:59:49 -0500

    Delete 02-IoC-Stereotype-Annotation project files
    All files related to the 02-IoC-Stereotype-Annotation project were deleted. This includes all Java class files, IML, XML, and configuration files used in the project.

--- 02-IoC-Stereotype-Annotation/src/main/java/com/cydeo/stereotype_annotation/config/PcConfig.java
+++
@@ -1,9 +0,0 @@
-package com.cydeo.stereotype_annotation.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@ComponentScan(basePackages = "com.cydeo")
-@Configuration
-public class PcConfig {
-}
