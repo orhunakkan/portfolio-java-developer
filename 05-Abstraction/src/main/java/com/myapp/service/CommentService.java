9e8ebb917353c6c366f7a828135f3c26219ea496
orhunakkan <orhunakkan@outlook.com>
2024-05-06 10:39:08 -0500

    Updated notification proxies and CommentService dependencies
    Added a new PushCommentNotificationProxy for handling push notifications. The existing EmailCommentNotificationProxy was updated for clarity. CommentService now utilizes the push notification proxy as a default dependency.

--- 05-Abstraction/src/main/java/com/myapp/service/CommentService.java
+++ 05-Abstraction/src/main/java/com/myapp/service/CommentService.java
@@ -3,6 +3,7 @@ package com.myapp.service;
 import com.myapp.model.Comment;
 import com.myapp.proxy.CommentNotificationProxy;
 import com.myapp.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,7 +12,7 @@ public class CommentService {
     private final CommentRepository commentRepository;
     private final CommentNotificationProxy commentNotificationProxy;
 
-    public CommentService(CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
+    public CommentService(CommentRepository commentRepository, @Qualifier("PUSH") CommentNotificationProxy commentNotificationProxy) {
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
     }
