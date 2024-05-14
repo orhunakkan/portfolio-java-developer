36d835018547021c0eb208e7212bcd06f65b245e
orhunakkan <orhunakkan@outlook.com>
2024-05-13 22:25:46 -0500

    Add DBConfigData and AppConfigData with updated application properties
    Added new DBConfigData and AppConfigData classes for the configuration of application and database. Updated application.properties file with new values and configurations for database. Also, modified the main method in MyApplication.java file to retrieve and print these new configurations.

--- 09-SpringBoot-App/src/main/java/com/myapp/MyApplication.java
+++ 09-SpringBoot-App/src/main/java/com/myapp/MyApplication.java
@@ -1,13 +1,36 @@
 package com.myapp;
 
+import com.myapp.config.AppConfigData;
+import com.myapp.config.DBConfigData;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.sql.SQLOutput;
 
 @SpringBootApplication
 public class MyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MyApplication.class, args);
+        ApplicationContext container =  SpringApplication.run(MyApplication.class, args);
+        String str = container.getBean(String.class);
+
+        AppConfigData appConfigData = container.getBean(AppConfigData.class);
+        System.out.println(appConfigData.getUsername());
+        System.out.println(appConfigData.getPassword());
+        System.out.println(appConfigData.getUrl());
+
+        DBConfigData data = container.getBean(DBConfigData.class);
+        System.out.println(data.getPassword());
+        System.out.println(data.getUsername());
+        System.out.println(data.getType());
+
+    }
+
+    @Bean
+    public String str1() {
+        return "ORHUN";
     }
 
 }
