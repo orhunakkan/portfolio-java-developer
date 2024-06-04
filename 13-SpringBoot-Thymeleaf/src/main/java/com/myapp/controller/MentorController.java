cb155323472e3c932d26c7e4f68b2d60ad61a0a7
orhunakkan <orhunakkan@outlook.com>
2024-06-03 23:47:48 -0500

    Add Mentor model, view and controller
    Added a new Mentor model with several properties like first name, last name, email, gender, batch, graduation status and company. Also extended MentorController with registration and confirmation methods, allowing mentors to be registered and their details confirmed. Corresponding Thymeleaf view templates (mentor-register.html and mentor-confirm.html) have been created to capture and display mentor data.

--- 13-SpringBoot-Thymeleaf/src/main/java/com/myapp/controller/MentorController.java
+++ 13-SpringBoot-Thymeleaf/src/main/java/com/myapp/controller/MentorController.java
@@ -1,19 +1,32 @@
 package com.myapp.controller;
 
+import com.myapp.model.Mentor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
+@RequestMapping("/mentor")
 public class MentorController {
 
-    @RequestMapping("/register")
-    public String register() {
-        return "student/register";
+    @GetMapping("/register")
+    public String register(Model model) {
+        model.addAttribute("mentor", new Mentor());
+        List<String> batchList = Arrays.asList("JD1", "JD2", "JD3");
+        model.addAttribute("batchList", batchList);
+        return "mentor/mentor-register";
     }
 
-    @RequestMapping("/drop")
-    public String drop() {
-        return "student/drop";
+    @PostMapping("/confirm")
+    public String confirm(@ModelAttribute ("mentor") Mentor mentor) {
+        System.out.println(mentor);
+//        return "mentor/mentor-confirm";
+        return "redirect:/mentor/register";
     }
-
 }
