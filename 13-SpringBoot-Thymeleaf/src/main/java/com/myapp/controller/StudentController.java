6c0edd5969d9ca3028a41f012c4cad680610f91e
orhunakkan <orhunakkan@outlook.com>
2024-06-03 20:36:42 -0500

    Refactor request mappings in StudentController
    Revised the request mappings in StudentController to specify HTTP request methods. Utilized @GetMapping and @PostMapping annotations for clearer distinction between GET and POST requests.

--- 13-SpringBoot-Thymeleaf/src/main/java/com/myapp/controller/StudentController.java
+++ 13-SpringBoot-Thymeleaf/src/main/java/com/myapp/controller/StudentController.java
@@ -4,8 +4,7 @@ import com.myapp.bootstrap.DataGenerator;
 import com.myapp.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -13,14 +12,16 @@ import java.util.List;
 @RequestMapping("/student")
 public class StudentController {
 
-    @RequestMapping("/register")
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    @GetMapping("/register")
     public String register(Model model) {
         List<Student> students = DataGenerator.createStudents();
         model.addAttribute("students", students);
         return "student/register";
     }
 
-    @RequestMapping("/welcome")
+    @RequestMapping(value ="/welcome", method = RequestMethod.POST)
+    @PostMapping("/welcome")
     public String welcomePage(@RequestParam String name) {
         System.out.println(name);
         return "student/welcome";
