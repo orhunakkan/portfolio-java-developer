beafdbc37bacc5473fa067d2da78c6381a3a4edb
orhunakkan <orhunakkan@outlook.com>
2024-06-16 00:19:44 -0500

    Implement Thymeleaf dynamic elements in templates
    Thymeleaf dynamic elements are added to the HTML templates in the SpringBoot-Thymeleaf-Lab, displaying data from the product list. Conditionally render sections in the 'list.html' file based on the size of the productList. Adding URL paths to the 'add to cart' and 'see cart' buttons in 'list.html'. Also, method to return the list view is added in ProductController.

--- 15-SpringBoot-Thymeleaf-Lab/src/main/java/myapp/controller/ProductController.java
+++ 15-SpringBoot-Thymeleaf-Lab/src/main/java/myapp/controller/ProductController.java
@@ -1,7 +1,14 @@
 package myapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProductController {
+
+    @GetMapping("/list")
+    public String getProductList() {
+        return "product/list";
+    }
+
 }
